3b481f5c17e707ac3f28c0a44c7b2f222ed83569
Hamseapdiasiis <hamse906@gmail.com>
2023-05-20 12:52:46 +0300

    Exercise done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let bill = 200;
+let tip = bill * 15/100;
+let tip1 = bill * 20/100;
+let value = bill + tip;
+let value1 = bill + tip1;
+
+if (bill > 50 && bill < 300)
+{
+    console.log(`the bill was ${bill}, the tip was ${tip}, and the total value is ${value}`);
+}else{
+    console.log(`the bill was ${bill}, the tip was ${tip1}, and the total value is ${value1}`);
+}
